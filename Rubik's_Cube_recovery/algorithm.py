cccbc51bc30994059656a286de752d8657d13205
Sukuna114514 <145947038+Sukuna114514@users.noreply.github.com>
2023-09-25 19:39:26 -0400

    Add files via upload
    Yuchen Zheng's Rubik's Cube restores the preliminary structure of part of the code. When uploaded for the first time, there is basically no content. The content will be completed in subsequent uploads.

---
+++ Rubik's_Cube_recovery/algorithm.py
@@ -0,0 +1,8 @@
+import kociemba
+def solve(cube_state: str) -> str:
+    try:
+        solution = kociemba.solve(cube_state)
+        return solution
+    except Exception as e:
+        # Error handling can be performed here, for example, when the Rubik's Cube status is illegal
+        raise ValueError(f"Cannot solve the cube. Error: {str(e)}")
