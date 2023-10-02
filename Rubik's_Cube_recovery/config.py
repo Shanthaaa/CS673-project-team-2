089b8025c9469b5dd1737698721aa22ddcdbaa4d
Sukuna114514 <145947038+Sukuna114514@users.noreply.github.com>
2023-10-02 12:31:45 -0400

    Update config.py

---
+++ Rubik's_Cube_recovery/config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# server configuration
+SERVER_HOST = "0.0.0.0"  # Can be set to "127.0.0.1" to allow local access only or "0.0.0.0" to allow access from all IPs
+SERVER_PORT = 5000
+DEBUG = True  # Can be set to True during development and False during production environment
+
+# API version
+API_VERSION = "v1"
+
+# Other possible configurations, such as database connection information, security keys, etc.
+# DATABASE_URI = "your-database-uri"
+# SECRET_KEY = "your-secret-key"
