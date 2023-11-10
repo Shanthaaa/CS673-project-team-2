fdd3ae860ec4d2ac168632ccf18fd1878a9e095e
Sukuna114514 <145947038+Sukuna114514@users.noreply.github.com>
2023-11-09 19:16:58 -0500

    Update algorithm.py

--- Rubik's_Cube_recovery/algorithm.py
+++ Rubik's_Cube_recovery/algorithm.py
@@ -1,10 +1,18 @@
 # algorithm.py
 
 import kociemba
+import random
+
 def solve(cube_state: str) -> str:
     try:
         solution = kociemba.solve(cube_state)
         return solution
     except Exception as e:
-        # Error handling can be performed here, for example, when the Rubik's Cube status is illegal
         raise ValueError(f"Cannot solve the cube. Error: {str(e)}")
+
+def generate_random_state(num_moves=25):
+    moves = ["U", "D", "L", "R", "F", "B", 
+             "U'", "D'", "L'", "R'", "F'", "B'", 
+             "U2", "D2", "L2", "R2", "F2", "B2"]
+    scramble = " ".join(random.choices(moves, k=num_moves))
+    return scramble
